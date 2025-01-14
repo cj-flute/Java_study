7f50571983ba7b155e8ac26544a3781b869f48f1
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-01-14 18:37:52 +0100

    Java board first push from my intelij ide

---
+++ CLass_board.java
@@ -0,0 +1,30 @@
+public class CLass_board {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.rollno = 1;
+        s1.name = "Dave";
+        s1.marks = 50;
+
+        Student s2 = new Student();
+        s2.rollno = 2;
+        s2.name = "Mike";
+        s2.marks = 70;
+
+        Student s3 = new Student();
+        s3.rollno = 5;
+        s3.name = "John";
+        s3.marks = 90;
+
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+    }
+}
+
+class Student{
+    int rollno;
+    String name;
+    int marks;
+}
