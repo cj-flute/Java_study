88a4d1dec5ae06b00064f352516b0b5f0c42ecc7
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-01-17 13:55:08 +0100

    Strings in java

--- CLass_board.java
+++ CLass_board.java
@@ -1,39 +1,6 @@
 public class CLass_board {
     public static void main(String[] args) {
-        int [] marks = new int[6];
-        marks[0] = 60;
-        marks[1] = 80;
-        marks[3] = 75;
-
-        for(int n : marks){
-            System.out.println(n);
-        }
-
-//        Student s1 = new Student();
-//        s1.rollno = 1;
-//        s1.name = "Dave";
-//        s1.marks = 50;
-//
-//        Student s2 = new Student();
-//        s2.rollno = 2;
-//        s2.name = "Mike";
-//        s2.marks = 70;
-//
-//        Student s3 = new Student();
-//        s3.rollno = 5;
-//        s3.name = "John";
-//        s3.marks = 90;
-//
-//        Student students[] = new Student[3];
-//        students[0] = s1;
-//        students[1] = s2;
-//        students[2] = s3;
-
+        String name = "Ezembadiwe Chijioke Michael";
+        System.out.println(name);
     }
-//    class Student{
-//        int rollno;
-//        String name;
-//        int marks;
-//    }
-
 }
