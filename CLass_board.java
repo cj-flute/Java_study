9908b71f7f50df9b47c32da71554b23fddd45d6c
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-01-17 14:10:24 +0100

    Strings in java

--- CLass_board.java
+++ CLass_board.java
@@ -1,6 +1,14 @@
 public class CLass_board {
     public static void main(String[] args) {
         String name = "Ezembadiwe Chijioke Michael";
+        name = name + "?";
         System.out.println(name);
+
+        // Is the strings s1 and s2 the same?
+        String s1 = "Navin";
+        String s2 = "Navin";
+
+        System.out.println(s1 == s2); // s1 and s2 string is pointing to the same string Navin. So they are the same.
+
     }
 }
